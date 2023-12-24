92afe3b265d0bcc4e97ec926936b7838fab2519a
Ando <iamando@tuta.io>
2023-12-24 20:15:41 +0300

    feat: add contrast on material colorsheme on sidebars and use transparency

--- .config/nvim/lua/iamando/plugins/colorscheme.lua
+++ .config/nvim/lua/iamando/plugins/colorscheme.lua
@@ -3,6 +3,17 @@ return {
     "marko-cerovac/material.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      local material = require("material")
+
+      material.setup({
+        contrast = {
+          sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        },
+        disable = {
+          background = true,
+        }
+      })
+
       -- load the colorscheme here
       vim.g.material_style = "darker"
       vim.cmd([[colorscheme material]])
