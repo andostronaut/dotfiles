4ee5b34eb71af50fcdc847984204324cfe031210
Ando <iamando@tuta.io>
2023-12-26 13:29:42 +0300

    feat: update lualine config on inactive session show empty for all

--- .config/nvim/lua/iamando/plugins/lualine.lua
+++ .config/nvim/lua/iamando/plugins/lualine.lua
@@ -89,6 +89,11 @@ return {
             path = 1,
           },
         },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     })
 
