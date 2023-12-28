ff57dd15ee24c89ceb6809012261c664acbd91c9
Ando <iamando@tuta.io>
2023-12-28 14:08:21 +0300

    feat: implement nvim auto session

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -391,6 +391,24 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+
+  -- auto session
+  {
+    "rmagatti/auto-session",
+    config = function()
+      local auto_session = require("auto-session")
+
+      auto_session.setup({
+        auto_restore_enabled = false,
+        auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      })
+
+      local keymap = vim.keymap
+
+      keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+      keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+    end,
+  },
 }
 
 local config = require("core.utils").load_config()
