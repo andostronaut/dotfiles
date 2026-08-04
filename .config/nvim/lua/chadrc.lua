0bfe99d047fca8ad2a2da1778952e1670213f5b0
Ando <hi@andostronaut.com>
2026-08-04 10:59:33 +0300

    Switch to Vesper across nvim, ghostty and herdr

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,8 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
-	theme_toggle = { "catppuccin", "catppuccin-latte" },
+	theme = "vesper",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
